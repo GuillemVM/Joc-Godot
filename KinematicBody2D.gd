e66e45e353911897c8942ee0705c1b3986ac2812
GuillemVM <79510025+GuillemVM@users.noreply.github.com>
2021-04-13 17:53:41 +0200

    programació del personatge principal

---
+++ KinematicBody2D.gd
@@ -0,0 +1,77 @@
+extends KinematicBody2D
+
+onready var _animated_player = $main_char
+
+const gravity = 10
+const up = Vector2(0, -1)
+const jump = -200
+const velocity = 100
+
+var moviment = Vector2()
+var attacking = false
+var death = false
+var direction = 1
+var grounded
+
+func _ready():
+	pass
+func _physics_process(delta):
+	if death == false:
+		if is_on_floor():
+			grounded = true
+		else:
+			grounded = false
+		moviment.y += gravity
+		if attacking == false:
+			moviment.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+			if grounded == true && Input.is_action_pressed("ui_up"):
+				moviment.y = jump
+			elif grounded == false && moviment.y < 0:
+				_animated_player.play("jump")
+			if moviment.x > 0:
+				direction = 1
+				if grounded == true:
+					_animated_player.play("walk")
+					_animated_player.flip_h = false
+			elif moviment.x < 0:
+				direction = -1
+				if grounded == true:
+					_animated_player.play("walk")
+					_animated_player.flip_h = true
+			elif moviment.x == 0:
+				if grounded == true:
+					_animated_player.play("idle")
+		if direction == 1:
+			$hitbox_main/left.disabled = true
+			$hitbox_main/right.disabled = false
+			$left.enabled = false
+			$right.enabled = true
+		elif direction == -1:
+			$hitbox_main/left.disabled = false
+			$hitbox_main/right.disabled = true
+			$left.enabled = true
+			$right.enabled = false
+		if Input.is_action_just_pressed("attack") && grounded == true:
+			attacking = true
+			_animated_player.play("attaack_1")
+			if direction == 1:
+				$hitbox_attack_1/left.disabled = true
+				$hitbox_attack_1/right.disabled = false
+			elif direction == -1:
+				$hitbox_attack_1/left.disabled = false
+				$hitbox_attack_1/right.disabled = true
+		moviment = move_and_slide(moviment, up)
+	elif death == true:
+		pass
+
+func _on_main_char_animation_finished():
+	if _animated_player.animation == "attack_1":
+		if direction == 1:
+			$hitbox_attack_1/right.disabled = true
+		elif direction == -1:
+			$hitbox_attack_1/left.disabled = true
+		attacking = false
+
+
+func _on_hitbox_main_area_entered(area):
+	pass
--- main.gd
+++
@@ -1,4 +0,0 @@
-extends KinematicBody2D
-
-func _physics_process(delta):
-	pass
